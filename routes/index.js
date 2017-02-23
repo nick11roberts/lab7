49e9042c9bc0b708d461b7ea9dcaa3a8d991a512
nick11roberts <n3robert@ucsd.edu>
2017-02-23 10:34:14 -0800

    Ready for experiment.

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,15 @@ var projects = require('../projects.json');
 /*
  * GET home page.
  */
-
 exports.view = function(req, res){
-  	res.render('index', projects);
+  projects['min'] = false;
+  res.render('index', projects);
+};
+
+/*
+ * GET home page alternate.
+ */
+exports.viewMin = function(req, res){
+  projects['min'] = true;
+  res.render('index', projects);
 };
